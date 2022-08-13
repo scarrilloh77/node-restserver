4b6352b5342f5656d581ceba199c4c4004e9beb0
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-13 00:57:59 -0500

    POST: Creando un usuario en la colección

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,4 +1,5 @@
 const { response, request } = require("express");
+const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
   // const query = req.query;
@@ -14,14 +15,13 @@ const usuariosGet = (req = request, res = response) => {
   });
 };
 
-const usuariosPost = (req, res = response) => {
-  // const body = req.body;
-  const { nombre, edad } = req.body;
+const usuariosPost = async (req, res = response) => {
+  const body = req.body;
+  const usuario = new Usuario(body);
+  await usuario.save();
   res.json({
     msg: "post API - controlador",
-    // body,
-    nombre,
-    edad,
+    usuario,
   });
 };
 
