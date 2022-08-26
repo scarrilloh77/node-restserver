c2d03819f39e8834ba765b1acf58e2fa0206851c
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-26 01:48:33 -0500

    Crear carpetas de destino

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -7,10 +7,16 @@ const cargarArchivo = async (req = request, res = response) => {
     return;
   }
 
-  const nombre = await subirArchivo(req.files);
-  res.json({
-    nombre,
-  });
+  try {
+    // txt, md
+    // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
+    const nombre = await subirArchivo(req.files, undefined, 'imgs');
+    res.json({
+      nombre,
+    });
+  } catch (msg) {
+    res.status(400).json({ msg });
+  }
 };
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -34,6 +34,7 @@ class Server {
       fileUpload({
         useTempFiles: true,
         tempFileDir: '/tmp/',
+        createParentPath: true,
       })
     );
   }
