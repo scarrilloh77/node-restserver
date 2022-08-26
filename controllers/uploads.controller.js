d2551374022ce64567ea3f93414b07c6dc9ddedd
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-25 23:46:57 -0500

    Validar la extensión

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -9,16 +9,29 @@ const cargarArchivo = (req = request, res = response) => {
   }
 
   const { archivo } = req.files;
+  const nombreCortado = archivo.name.split('.');
+  const extension = nombreCortado[nombreCortado.length - 1];
 
-  const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+  // Validar la extension
+  const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
 
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
+  if (!extensionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `La extensión ${extension} no es permitida, ${extensionesValidas}`,
+    });
+  }
 
-    res.json({ msg: 'File uploaded to' + uploadPath });
-  });
+  res.json({ extension });
+
+  // const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+
+  // archivo.mv(uploadPath, (err) => {
+  //   if (err) {
+  //     return res.status(500).json({ err });
+  //   }
+
+  //   res.json({ msg: 'File uploaded to' + uploadPath });
+  // });
 };
 
 module.exports = {
