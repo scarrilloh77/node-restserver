258d3ca585411bdacddecc5f72d0f559ff43b19a
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-27 01:44:49 -0500

    Borrar imágenes de Cloudinary

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -98,8 +98,14 @@ const actualizarImagenCloudinary = async (req = request, res = response) => {
       return res.status(500).json({ msg: 'Se me olvido validar esto' });
   }
 
+  // Limpiar imágenes previas
   if (modelo.img) {
+    const nombreArr = modelo.img.split('/');
+    const nombre = nombreArr[nombreArr.length - 1];
+    const [public_id] = nombre.split('.');
+    cloudinary.uploader.destroy(public_id);
   }
+
   const { tempFilePath } = req.files.archivo;
   const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
   modelo.img = secure_url;
